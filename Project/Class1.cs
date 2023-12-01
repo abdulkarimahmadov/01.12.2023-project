49601b8ee52149b2743a71ef14a268786a6a8efe
Abdulkarim Ahmadov <148267382+abdulkarimahmadov@users.noreply.github.com>
2023-12-01 23:32:20 +0400

    Project
    (opsiyonel)

---
+++ Project/Class1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace MyCustomSpace
+{
+    public class CustomGenList<T>
+    {
+        private T[] items;
+        private int count;
+        private int capacity;
+
+        public int Count
+        {
+            get { return count; }
+        }
+
+        public int Capacity
+        {
+            get { return capacity; }
+        }
+
+        public CustomGenList()
+        {
+            items = new T[0];
+            count = 0;
+            capacity = 0;
+        }
+
+        public void Add(T item)
+        {
+            if (count == capacity)
+            {
+                if (capacity == 0)
+                    capacity = 4;
+                else
+                    capacity *= 2;
+
+                T[] newItems = new T[capacity];
+                for (int i = 0; i < count; i++)
+                {
+                    newItems[i] = items[i];
+                }
+
+                items = newItems;
+            }
+
+            items[count] = item;
+            count++;
+        }
+
+        public T Find(Predicate<T> match)
+        {
+            return Array.Find(items, match);
+        }
+
+        public CustomGenList<T> FindAll(Predicate<T> match)
+        {
+            T[] foundItems = Array.FindAll(items, match);
+            CustomGenList<T> resultList = new CustomGenList<T>();
+
+            foreach (T item in foundItems)
+            {
+                resultList.Add(item);
+            }
+
+            return resultList;
+        }
+
+        public bool Contains(T item)
+        {
+            return Array.IndexOf(items, item) >= 0;
+        }
+
+        public bool Exists(Predicate<T> match)
+        {
+            return Array.Exists(items, match);
+        }
+
+        public void Remove(Predicate<T> match)
+        {
+            int newIndex = 0;
+            for (int i = 0; i < count; i++)
+            {
+                if (!match(items[i]))
+                {
+                    items[newIndex] = items[i];
+                    newIndex++;
+                }
+            }
+
+            count = newIndex;
+        }
+    }
+}
+
---
+++ Project/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace MyCustomSpace
+{
+    class Program
+    {
+        static void Main()
+        {
+            CustomGenList<int> customList = new CustomGenList<int>();
+
+            // add lari test edriy
+            customList.Add(1);
+            customList.Add(2);
+            customList.Add(3);
+            customList.Add(4);
+
+            // count ve capacity test
+            Console.WriteLine($"Count: {customList.Count}, Capacity: {customList.Capacity}");
+
+            // add methodun yoxlayiriq capacity artanda
+            customList.Add(5);
+            Console.WriteLine($"Count: {customList.Count}, Capacity: {customList.Capacity}");
+
+            // find-i yoxlamaq
+            Console.WriteLine($"Find(3): {customList.Find(x => x == 3)}");
+
+            // test FindAll
+            CustomGenList<int> findAllResult = customList.FindAll(x => x % 2 == 0);
+            Console.WriteLine($"FindAll(even): {string.Join(", ", findAllResult)}");
+
+            // test Contains
+            Console.WriteLine($"Contains(2): {customList.Contains(2)}");
+
+            // test Exists 
+            Console.WriteLine($"Exists(odd): {customList.Exists(x => x % 2 != 0)}");
+
+            // test Remove
+            customList.Remove(x => x == 3);
+            Console.WriteLine($"After removing 3, Count: {customList.Count}, Capacity: {customList.Capacity}");
+        }
+    }
+}
